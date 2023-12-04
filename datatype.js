20b5b1349544c7c0e690ba8b92ee09872d9743f0
Navanit vishwakarma <98607461+navanitvish@users.noreply.github.com>
2023-12-04 16:31:38 +0000

    DataTypes

---
+++ datatype.js
@@ -0,0 +1,16 @@
+console.log("Navanit");
+console.log("12345");
+let name = "Navanit";
+let age = 20;
+let signIn=true;
+
+console.table ([name,age,signIn]);
+
+// number = 2 to power 53
+// bigint = for Big Intergers ya big number like trending, stockmarket 
+// Boolean = true, false
+// null = standlone value hai means khali value or empty value
+// undefined = value
+// symbol= unique ke liye
+// object
+console.log(typeof age);
